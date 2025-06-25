b6c5b62f9039f1d7771c13e3603a9e23938a00fa
theprakashkumar <maiprakashkumar@gmail.com>
2025-06-25 15:51:22 +0530

    feat: Integrate Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  // Handle path alias that we have added in vite config.
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+
+    return config;
+  },
+};
+export default config;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,11 @@
-const Button = () => {
-  return <button className="text-blue-600">Something</button>;
+import { type ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button className="text-blue-600 border-amber-500 border" {...props} />
+  );
 };
 
 export default Button;
