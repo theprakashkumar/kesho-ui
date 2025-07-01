4bf1e1f8be235b04138195c9fd433add603fb8c4
theprakashkumar <maiprakashkumar@gmail.com>
2025-07-01 17:26:10 +0530

    work in progress

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,57 @@
+import { cva, type VariantProps } from "class-variance-authority";
 import { type ComponentProps } from "react";
 
-type ButtonProps = ComponentProps<"button">;
+const buttonStyles = cva(
+  [
+    "w-full",
+    "rounded",
+    "semi-bold",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+  ],
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "border-2 border-blue-200 bg-white",
+        ghost: "transition-colors duration-300",
+      },
+      size: {
+        sm: "px-4 py-2 text-sm",
+        md: "px-4 py-3 text-base",
+        lg: "px-6 py-3 text-lg",
+      },
+      colorScheme: {
+        primary: "text-white",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "primary",
+        className: "bg-primary-500 hover:bg-primary-600",
+      },
+      {
+        variant: "outline",
+        colorScheme: "primary",
+        className:
+          "text-primary-600 border-primary-500 bg-transparent hover:bg-blue-100",
+      },
+      {
+        variant: "ghost",
+        colorScheme: "primary",
+        className: "text-primary-600 bg-transparent hover:bg-primary-100",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+      colorScheme: "primary",
+    },
+  }
+);
+
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
 
 const Button = ({ ...props }: ButtonProps) => {
   return (
